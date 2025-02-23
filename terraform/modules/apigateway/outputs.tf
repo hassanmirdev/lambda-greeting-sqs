02d022b6410f726be3af34b019462830848f0106
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-23 12:40:32 +0530

    Update outputs.tf

--- terraform/modules/apigateway/outputs.tf
+++ terraform/modules/apigateway/outputs.tf
@@ -1,6 +1,3 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
 output "greeting_api_endpoint" {
   value = "${aws_api_gateway_deployment.greeting_api_deployment.invoke_url}/greet"
 }
