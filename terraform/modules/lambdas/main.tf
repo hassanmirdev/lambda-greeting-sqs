f8ed8ea7c7be5cff78883f40f6242c73f641bacd
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-23 12:41:08 +0530

    Update main.tf

--- terraform/modules/lambdas/main.tf
+++ terraform/modules/lambdas/main.tf
@@ -1,6 +1,3 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-
 resource "aws_iam_role" "greeting_lambda_execution_role" {
   name = "greeting_lambda_execution_role"
 
