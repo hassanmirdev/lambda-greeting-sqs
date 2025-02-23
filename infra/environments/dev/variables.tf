8a28e3670eb5a4b4a15835ea5fee573548251757
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-23 14:45:37 +0530

    Update variables.tf

--- infra/environments/dev/variables.tf
+++ infra/environments/dev/variables.tf
@@ -1,9 +1,11 @@
 variable "environment" {
   type = string
+  description = "define environment variable"
   default = "production"
 }
 
 variable "lambda_memory_size" {
+  description = "define lambda memory size"
   type = number
   default = 1024
 }
