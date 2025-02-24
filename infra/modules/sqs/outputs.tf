9e935c48037861835f98694cf5f1c44c4a215395
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-24 15:07:39 +0530

    Update outputs.tf

--- infra/modules/sqs/outputs.tf
+++ infra/modules/sqs/outputs.tf
@@ -1,3 +1,5 @@
+# This code outputs the ARN and name of the greetings_queue SQS queue for use in other resources or for reference
+
 output "greeting_queue_arn" {
   value = aws_sqs_queue.greeting_queue.arn
 }
