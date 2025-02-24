472548185c65c1f93f3a7f70ae0bacc6870a3e5b
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-24 15:06:41 +0530

    Update main.tf

--- infra/modules/sqs/main.tf
+++ infra/modules/sqs/main.tf
@@ -1,3 +1,5 @@
+# Creates an SQS queue with SSE (Service Side Encryption) enabled and tags it with the environment variable for management.
+
 resource "aws_sqs_queue" "greeting_queue" {
   name                    = "greetings_queue"
   sqs_managed_sse_enabled = true
