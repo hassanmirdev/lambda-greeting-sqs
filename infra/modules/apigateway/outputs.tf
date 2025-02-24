3a0775801a01c814f640200c445170746f2fcd5b
Hassan Mir <59356530+hassanmirdev@users.noreply.github.com>
2025-02-24 14:53:18 +0530

    Update outputs.tf

--- infra/modules/apigateway/outputs.tf
+++ infra/modules/apigateway/outputs.tf
@@ -1,3 +1,5 @@
+# This outputs the full endpoint URL for the deployed greeting_api API Gateway, appending /greet to the invoke URL.
+
 output "greeting_api_endpoint" {
   value = "${aws_api_gateway_deployment.greeting_api_deployment.invoke_url}/greet"
 }
